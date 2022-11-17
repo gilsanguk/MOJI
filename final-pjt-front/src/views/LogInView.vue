1696a124a65e33fab44d4391636cb1c64ad7fee8
송은지 <cjsdks518@naver.com>
2022-11-17 16:04:03 +0900

    css 로그인

--- final-pjt-front/src/views/LogInView.vue
+++ final-pjt-front/src/views/LogInView.vue
@@ -1,15 +1,22 @@
 <template>
-  <div id="logindiv" class="d-flex align-items-center justify-content-end">
-    <div id="loginform">
-      <h1>LogIn</h1>
-      <form @submit.prevent="logIn">
-        <label for="username">username : </label>
-        <input type="text" id="username" v-model="username"><br>
-
-        <label for="password"> password : </label>
-        <input type="password" id="password" v-model="password"><br>
-
-        <input type="submit" value="logIn">
+  <div id="logindiv">
+    <div id="loginformdiv">
+      <div>
+        <h1 class="mb-3 text-left"><b>LogIn</b></h1>
+      </div>
+      <form @submit.prevent="logIn" id="loginform">
+        <div class="d-flex flex-column">
+          <label for="username" id="username">username : </label>
+          <input type="text" id="username" placeholder="아이디를 입력하시오." v-model="username" />
+        </div>
+        <div class="d-flex flex-column">
+          <label for="password" id="password"> password : </label>
+          <input type="password" id="password" placeholder="비밀번호를 입력하시오." v-model="password" />
+        </div>
+        <div>
+          <span id="clickbutton" >회원가입</span>
+          <input type="submit" value="logIn" id="clickbutton" />
+        </div>
       </form>
     </div>
   </div>
@@ -17,50 +24,95 @@
 
 <script>
 export default {
-  name: 'LogInView',
+  name: "LogInView",
   data() {
     return {
       username: null,
       password: null,
-    }
+    };
   },
   methods: {
     logIn() {
       const user = {
         username: this.username,
         password: this.password,
-      }
-      this.$store.dispatch('logIn', user)
-    }
-  }
-}
+      };
+      this.$store.dispatch("logIn", user);
+    },
+  },
+};
 </script>
 
 <style scoped>
-#logindiv{
-  height: 100vh;
-  background: url('https://assets.nflxext.com/ffe/siteui/vlv3/9737377e-a430-4d13-ad6c-874c54837c49/945eec79-6856-4d95-b4c6-83ff5292f33d/KR-ko-20220111-popsignuptwoweeks-perspective_alpha_website_large.jpg');
+label#username,
+label#password {
+  width: 400px;
+  height: 15px;
+  border-radius: 10px;
+  padding-left: 1rem;
+  margin-top: 1rem;
+  display: inline-block;
+  text-align: left;
+  vertical-align: middle;
 }
 
-#logindiv:before{
+input#username,
+input#password {
+  width: 400px;
+  height: 38px;
+  border-radius: 10px;
+  padding-left: 1rem;
+  margin-top: 1rem;
+  display: inline-block;
+  text-align: left;
+  vertical-align: middle;
+}
+
+#clickbutton {
+  width: 70px;
+  height: 38px;
+  border-radius: 10px;
+  margin-left: 2rem;
+  margin-top: 1rem;
+}
+
+#logindiv {
+  height: 100vh;
+  background: url("https://assets.nflxext.com/ffe/siteui/vlv3/9737377e-a430-4d13-ad6c-874c54837c49/945eec79-6856-4d95-b4c6-83ff5292f33d/KR-ko-20220111-popsignuptwoweeks-perspective_alpha_website_large.jpg");
+  display: flex;
+  align-items: center;
+  justify-content: end;
+}
+
+#logindiv:before {
   margin: 0;
-  content: '';
-  background: linear-gradient(to left, rgba(0, 0, 0, 0.733),transparent);
+  content: "";
+  background: linear-gradient(to left, rgba(0, 0, 0, 0.733), transparent);
   position: absolute;
   left: 0;
   height: 100%;
   width: 100%;
 }
 
-#loginform{
-  width: 25%;
-  height: 25%;
+#loginformdiv {
+  padding: 2rem;
   background-color: rgba(0, 0, 0, 0.447);
-  color : white;
+  color: white;
+  overflow: hidden;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   position: fixed;
-  margin-right: 5rem;
+  margin-right: 15rem;
+}
+
+#loginform {
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  justify-content: center;
 }
 </style>
